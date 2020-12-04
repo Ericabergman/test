437a2c3fabe462c21dee7c0324c33831a5430b29
Erica Bergman <70317153+Ericabergman@users.noreply.github.com>
2020-12-04 13:10:23 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,477 @@
+// let js = "awsome";
+// if (js === "awsome") alert(" javascript is the best!");
+
+
+// 20 + 10;
+
+// console.log(20 + 8 + 23 - 10);
+
+// let  lesson = "first";
+
+// "Erica"
+// console.log ("Erica NY");
+// console.log ("Erica NY");
+// console.log ("Erica NY");
+// console.log ("Erica NY");
+
+// 53
+
+// let firstName camelcase
+
+// firstname = veriabel
+
+// string exempelvis= let  firstName = "erica";  always put then in quotes.
+
+// boolin kan bara bli sant eller falskt
+
+
+// declaring a veriabel and assigment a value let lesson = "first";
+
+
+// hon vill att vi förklarat vår kod eller skriver kommerntar i vårt arbete / code för att öva 
+
+
+// 1. ge min veriabel ett namn
+// 2. ge min veriabel ett 
+
+
+// operater=operatör
+
+
+
+// true;
+// console.log(true);
+
+
+// let youwilllearnJs = true;
+// console.log(youWilllearnJs);
+
+// youWilllearnJs = false;
+// console.log(youWilllearnJs);
+
+// console.log(typeof "Erica"); // data typen och värdet Helena
+// console.log(typeof 35); //data typen nummer och värdet 35
+// console.log(typeof youWilllearnJs); // data typen boolean och värdet false
+
+// console.log(typeof "Erica"); // data typen och värdet Helena
+// console.log(typeof 35); //data typen nummer och värdet 35
+// console.log(typeof youWilllearnJs); // data typen boolean och värdet false 
+
+
+// + är en operater och - är en operatöer används för att se ett spesicift resultat eller värde
+
+
+// används för att andra värde på variabler.
+// kan avnväda samma operatör för att ändra värde 
+
+
+// dynamiskt
+
+// let firstName = "Erica";
+// console.log(firstName);
+
+// let firstName = "Sara";
+
+
+// console.log(firstName);
+
+
+// let firstName = 35;
+
+// let lastName = "sara"
+
+
+// let day;// declarerat en veriabel värdet=undefined
+// console.log(day);
+
+// day = "wensday"; //wensday anget värder tll veriabel
+// console.log(day);
+
+
+
+// //errors i javascript=buggar.finns buggar i mer eller mindre alla språk.  typeof bugg som inte blir rätt. det påverkar ingen men ändå bra att veta om 
+
+
+// console.log(typeof null); //ej ett objekt som consolen visar. 
+
+
+
+
+
+// prata om let
+
+// LET CONST VAR 
+// som vi anväder för att deklarera en veriabel
+// man vill bugga som code så säker som möjligt
+// kan bli problem om man deklarerar sina veriabler let
+// rutin att declarera våre veriabler med const och inte som let
+// använd bara let om det är ett måste 
+// börja alltid med const och inte let 
+
+// declarere första cången 
+// let kan ändra värde senare 
+// med const kan man det inte 
+// använd framför atllt const men exempelvis födelseår ska inte var let
+
+
+//best practise, använd cosnt
+
+
+//VAR användes förr men inte längre
+//var var det gamla sättet att deklare veriabler på
+
+// vi ska förstå skillnade mellan let och const 
+// och när man använder vad
+// det finns många olika operatörer
+
+
+
+
+// const me  = "Helena";
+// me = "Hej";
+
+
+
+// const birthYear = 1994;
+
+
+
+// nu ska co kolla på operatörer
+// mathematic, compasison, logical, assigment etc.
+
+
+//
+
+// const birthYear = 2020 - 1985;
+// console.log(birthYear);
+
+// const brothersage = 2020 - 1991;
+// console.log(brothersAge);
+
+
+// const now = 2020;
+// const myAge = now - 1985;
+// const brothersAge = now - 1991;
+
+// console.log(myAge > brothersAge);
+
+
+
+// console.log(myAge, brothersAge);
+
+// console.log(myAge * 2, myAge / 10)
+
+// console.log(myAge * 3, brothersAge);
+
+// let X = 10 + 5; // x= 15
+// console.log(x);
+// x +=10; // x = x + 10 värdet här är 25
+
+// console.log(x);
+// x *= 4; // x = * 4 = 100
+// dynamiska konventerar automatiskt datattper. en string kan bli en booling
+
+
+
+// const isFullAge = 15;
+
+// const isFullAge = sarasAge >= 18;
+// console.log(isFullAge);
+// console.log(typeof isFullAge);
+
+// const now = 2020;
+
+// console.log(now - 1991 > now - 1985);
+
+// 2020 - 1991 om resultatet är sörre än 2020 - 1989
+
+
+
+
+
+
+
+//LESSON 2
+
+//oporator precedence finns på github att kolla på
+// högst värde utgors först
+// lägst värde utgörs sist 
+
+
+// opreateringslista med värden. 
+// vi behöver bara veta vilken som har höre värde än den andra, inte varför det är så.
+// behöver ha koll på vilken som konventraar först annars behöver vi iye memorera något 
+
+
+
+
+
+// exempel 
+// const now = 2020;
+// const myAge = now - 1985
+// const brothersAge = now - 1991
+// console.log(now - 1991 > now -1985);
+
+
+//- utförs först och förjer vänster till höger. 
+
+
+// let x, y;
+// x = y = 25 - 10 -5; // x = y = 10
+// console.log(x, y);
+// // först 10 = y 
+// //värdet på y är = 10 x = y = 10
+
+
+// detta är inget vi kommer att jobba med sedan. detta är bara för att illustrera för oss hur det fungerar. 
+// ett tips är att spara tabellen och kolla på vid behov. 
+
+
+//prioriterar operatörer (ordningen)  först
+
+// räkna ut medelålder. 
+
+//const averageAge = (myAge + brothersAge) / 2;
+//const averageAge = myAge + brothersAge / 2;
+//console.log(myAge, averageAge);
+//const averageAge 
+
+
+
+
+
+
+
+
+//veriabel har färg ljusblått
+//strings och template literals
+
+// "" eller ''    vi ska börja använda från och med nu
+
+//const firstName = "Erica";
+//const job = "Student"
+//const birthYear = 1994;
+//const now = 2020;
+
+//const Erica = "i'm"
+
+
+
+// template literals = ett sätt att skriva på 
+//const newErica = `i'm ${firstName}, a ${now - birthYear} years old ${job}`;
+//   back ticks=(``) håll oss innanför backticksen 
+//const newErica = i'm `${firstName}`
+
+//console.log(newErica);
+
+
+// \n\ ny rad= backslash;
+
+
+
+
+
+
+// Ta beslut i vår kod TALKING DESICCION 
+// strukturera innan börja koda
+
+// exempel: ta ta körkort eller inte.
+// Tips: bryt ner saker i små delar
+// 1. först behöver vi skapa en veriabel so innheåller värdet av en ålder
+// 2. sedan behöver vi skapa en verabel för ålder tt ta körkort
+// 3. jämföra om veriabel 1 är = eller högre än veriabel 2
+// 4. ta beslut beroende på om beslutet är sant eller falskt
+
+//isOfAgekolla om detta är sant
+//if = om
+
+//const age = 19;
+//const isOfAge = age >= 18;
+
+// if (isOfAge) {
+//     console.log("YAY! you can take your drivers licens!!!");
+// } else { 
+//     console.log("sorry come back in a few years!");
+// }
+
+
+
+//scope = declarera på rätt ställe
+//innanför barckets lever bara innan för det ScopedCredentialinte utanför. 
+// {} = scope, variabler som är deklarerade innanför dessa lever endast där
+
+// let century;
+//  if (birthYear <= 2000) { 
+
+//      century = 20; 
+//  } else {
+
+//      century = 21;
+//  }
+// console.log(century);
+
+/*
+const firstName = "Erica";
+const job = "Student";
+const birthYear = 1994;
+const now = 2020;
+
+const Erica = "i'm" 
+
+const newErica = `i'm ${firstName}, a ${now - birthYear} years old ${job}`;
+
+*/
+
+
+
+
+
+// dynamiska värden = vi behöver inte tala om för javascript att vi ska konvertera
+
+// TYPE COERSION & CONVERSION
+
+// TYPE COERSION = manuellt
+/*
+const inputYear = '1993';    //'allt innanför'=string 
+console.log(typeof inputYear);
+
+console.log(inputYear + 15);
+
+console.log(Number(inputYear) + 15);
+
+console.log(String(25));
+
+
+// CONVERSION = automatiskt
+console.log("i am " + 35 + " years old");
+
+console.log('23' - '10' - 3); // COERISON
+console.log('a' - '10' - 3); 
+console.log('23' + '10' + 3);// NO COERISON
+
+console.log('23' > '18');
+*/
+/*
+let n = '1' + 1; // 11
+// jag tar 1 som är en sträng och skriver ut: 1 1 och lägger till nummer 1
+n = n -1; // 10
+console.log(n);
+*/
+
+/*
+// THRUTHY & FALSE VALUES
+// FINNS 5 FALSY VALUES: 0,'', undefined, NaN, null
+
+
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean('Erica'));
+// console.log(Boolean({})); 
+*/
+/*
+const money = 0;
+
+if (money) {
+    console.log("Don´t spend it all!");
+} else {
+    console.log("You should get a job!");
+}
+
+let height;
+if (height) {
+    console.log("YAY! it´s defined");
+} else {
+    console.log("height is undefied");
+}
+*/
+
+// EQUALITY OPERATORS 
+/*
+const age = '18';
+if (age === 18)console.log("you just became an adult!");
+// 18 === 18
+
+// 3 equal is strict will never use type coersion 
+// 2 aqual does actually do type coersoin 
+
+// '18' == 18 undvik 2 använd alltid strict annars blir det buggar. Undvik så mycket det bara går
+
+const number = 25;
+if (number !== 20)console.log("hejhej");
+använd !==
+
+*/
+
+
+
+
+
+
+// TEORI Boolean Logic 
+//teoretiskt idag, kod imon
+
+
+
+
+
+
+
+// first  assigment 
+
+// vi har 2 lag som ska tävla mot varandra. lagen kommer 
+// spela tre rundor. Laget med högsta poäng vinner!
+// DATA O: 96, 108, 89
+// DATA ALLIANCE 88, 91, 110
+// 1. RÄckna ut medelvärde för varje lag. plussa ihop varje lags resultat
+// dela resultatet på 2
+// 2. jämföra de båda lagens resultat för att jämföra vilket som är högst 
+// för att kunna jämföra resultaten behöver vi använda större operator
+// vi behöver använda en if-sats som skreiver ut resultatet
+// är vinnaren.
+
+// steg 1: två variabler som inehåller varje lags data 
+// steg 1.1: skapa två nya veriabler som innehåller varje lags data 
+// steg 2: skapa en if-sats och avgöra vilket medelvärde som
+
+
+
+
+//Gör detta först. gör en del i taget
+//const result = 10 + 10 + 10; veriabler //är det med ljusblåt och döp dom beskrivande men kort och vettigt. //gör detta är bättre än att inte göra någon ting alls
+//const result2 = 5 + 5 + 5; veriabler //gör detta är bättre än att inte göra någon ting alls
+
+// const avrageResult = result / 2;
+// const avrageResult2 = result2 / 2;
+
+
+//Börja med att döpa veriabel med först const ellet let
+//const sen veriabel
+//let sen veriabel
+
+
+//console.log(result, result2);
+//console.log(avrageResultat, avrageResultat)
+
+// if (avrageResult > avrageResult2) {
+//     console.log("GRATTIS OG");
+// } else {
+//     console.log("Grattis ALLIANCE");
+// }
+
+
+
+
+// 1: först definera du vad du ska utföra
+// 2: definerar du vilka steg rentkodmässigt du behöver genomföra
+// 3: svriv ner vilken ordning du behöver utföra stegen.
+// 4: till en början och kom tillbaka till det sen.
+
+
+
+
+
+
+
+
+
+//Lektion 3
